421cc00992b5efef3a805ac56f340255910392a5
M.Yudith Aryanta Aditya <95072812+yudthadtyaaa@users.noreply.github.com>
2023-03-07 20:46:35 +0800

    Add files via upload

---
+++ POSTTEST1/src/App.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        // contoh perulangan for
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Ini adalah perulangan ke-" + i);
+        }
+        
+        // contoh perulangan while
+        int j = 1;
+        while (j <= 5) {
+            System.out.println("Ini adalah perulangan ke-" + j);
+            j++;
+        }
+        
+        // contoh perulangan do-while
+        int k = 1;
+        do {
+            System.out.println("Ini adalah perulangan ke-" + k);
+            k++;
+        } while (k <= 5);
+    }
+}
+
